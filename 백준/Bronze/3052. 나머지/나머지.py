02effb409f1d6bc823f23885fe2b3b241de02dc2
eunji <aggee212@gmail.com>
2025-09-04 19:30:47 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,7 +1,13 @@
-rest = []
-for _ in range(10):
-    N = int(input())
-    if N % 42 not in rest:
-        rest.append(N % 42)
+nums = [0] * 10
+rests = [0] * 42    # 41 인덱스 까지 사용
+cnt = 0
+# 입출력
+for i in range(10):
+    nums[i] = int(input())
 
-print(len(rest))
+for num in nums:
+    if rests[num % 42] == 0:
+        rests[num % 42] += 1
+        cnt += 1
+
+print(cnt)
