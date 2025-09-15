13fc809cf23f3857bf9eb542af3002e37574e611
eunji <aggee212@gmail.com>
2025-09-15 21:37:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 296 ms, Memory: 33544 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,37 +1,35 @@
-N, M, V = map(int, input().split())
-graph =[[] for _ in range(N+1)]
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-for i in range(1, N+1):
-    graph[i].sort()
-
-# dfs 풀이
-visited_d = [False] * (N + 1)
 def dfs(v):
-    visited_d[v] = True
     print(v, end=' ')
-    for nxt in graph[v]:
-        if not visited_d[nxt]:
-            dfs(nxt)
+    visited_dfs[v] = True
+    for i in graph[v]:
+        if visited_dfs[i] == False:
+            dfs(i)
 
-
-# bfs 풀이
-visited_b = [False] * (N + 1)
-def bfs(start):
-    q = [start]
-    visited_b[start] = True
+def bfs(n):
+    q = [n]
+    visited_bfs[n] = True
     while q:
         v = q.pop(0)
         print(v, end=' ')
-        for nxt in graph[v]:
-            if not visited_b[nxt]:
-                visited_b[nxt] = True
-                q.append(nxt)
+        for i in graph[v]:
+            if visited_bfs[i] == False:
+                visited_bfs[i] = True
+                q.append(i)
 
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+
+for g in graph:
+    g.sort()
+
+visited_dfs = [False] * (N+1)
+visited_bfs = [False] * (N+1)
 dfs(V)
 print()
 bfs(V)
