c35916506c28982bda00959f9e42930b8d13a558
eunji <aggee212@gmail.com>
2025-09-21 02:55:01 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 312 ms, Memory: 46504 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    arr.append((x, y))
+
+arr.sort()
+for i in range(N):
+    print(*arr[i])
