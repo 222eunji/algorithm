c962df6d2861f89264a25e83a61bf727b7a89448
eunji <aggee212@gmail.com>
2025-09-29 03:30:32 +0900

    [D2] Title: 파리 퇴치, Time: 59 ms, Memory: 51,584 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -4,16 +4,25 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     grid = [list(map(int, input().split())) for _ in range(N)]
 
-    # 완전탐색
-    max_catch = 0
-    for i in range(N-M+1):
-        for j in range(N-M+1):
-            catch = 0
-            for r_idx in range(M):
-                for c_idx in range(M):
-                    catch+=grid[i+r_idx][j+c_idx]
+    # 1. prefix 배열 초기화
+    prefix = [[0] * (N+1) for _ in range(N+1)]
 
-            if max_catch < catch:
-                max_catch = catch
+    # 2. prefix 채우기
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            prefix[i][j] = (grid[i-1][j-1]
+                            + prefix[i-1][j]
+                            + prefix[i][j-1]
+                            - prefix[i-1][j-1])
+
+    # 3. 파리채 잡기
+    max_catch = 0
+    for i in range(M, N+1):
+        for j in range(M, N+1):
+            total = (prefix[i][j]
+                     - prefix[i-M][j]
+                     - prefix[i][j-M]
+                     + prefix[i-M][j-M])
+            max_catch = max(total, max_catch)
 
     print(f'#{tc} {max_catch}')
