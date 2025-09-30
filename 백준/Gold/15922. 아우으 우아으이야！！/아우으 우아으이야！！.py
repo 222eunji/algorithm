63b9f41153c8510fe097a5b32cc10ce8e4240a97
eunji <aggee212@gmail.com>
2025-09-30 22:31:11 +0900

    [Gold V] Title: 아우으 우아으이야!!, Time: 2604 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.py
@@ -0,0 +1,16 @@
+
+N = int(input())
+# 초기값 설정
+x0, y0 = map(int, input().split())
+length = 0
+for _ in range(N-1):
+    x, y = map(int, input().split())
+    if x <= y0 and y >= y0:
+        y0 = y
+    elif x > y0:
+        length += (y0-x0) # 길이 더해주고
+        x0, y0 = x, y   # 초기화
+length += (y0-x0)   # 마지막 값 더해주기
+print(length)
+
+
