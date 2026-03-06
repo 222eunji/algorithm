839963787e97db62d1ba7de0105e2550680eb9e8
eunji <aggee212@gmail.com>
2026-03-06 20:21:41 +0900

    [Gold IV] Title: 타일 채우기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -1,14 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
-dp = [0]*(n+1)
+dp = [0] * (n+1)
+dp[0] = 1
 
-if n % 2 != 0:
-    print(0)
-else:
+if n >= 2:
     dp[2] = 3
-    for i in range(4, n+1, 2):
-        dp[i] = dp[i-2] * 3 + 2
-        for j in range(2, i-2, 2):
-            dp[i] += dp[j] * 2
 
-    print(dp[n])
+for i in range(3, n+1):
+    if i % 2:   # 홀수
+        pass
+    else:       # 짝수
+        dp[i] = 3*dp[i-2]
+        for j in range(4, i+1, 2):
+            dp[i] += 2*dp[i-j]
+
+print(dp[n])
