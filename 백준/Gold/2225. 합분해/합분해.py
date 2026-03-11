95c7a3f607d907d054b4e0029890129752abb1f8
eunji <aggee212@gmail.com>
2026-03-11 21:49:09 +0900

    [Gold V] Title: 합분해, Time: 252 ms, Memory: 114332 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,15 +1,18 @@
 import sys
- 
 input = sys.stdin.readline
- 
-n,k = map(int,input().split())
-mod = 1000000000
-d=[[0]*(k+1) for _ in range(n+1)]
- 
-for i in range(n+1):
-    for j in range(1,k+1):
-        if i==0:
-            d[i][j]=1
-        else:
-            d[i][j] = (d[i-1][j]+d[i][j-1])%mod
-print(d[-1][-1])
+
+N, K = map(int, input().split())
+# dp = list([0] * K for _ in range(N + 1))
+dp = [[0] * (N+1) for _ in range(K + 1)]
+
+# 1개로 N 만들기
+for i in range(N+1):
+    dp[1][i] = 1
+
+# K개로 N 만들기
+for k in range(2, K+1):
+    for i in range(0, N+1):
+        for j in range(0, i+1):
+            dp[k][i] += dp[k-1][j]
+# print(dp)
+print(dp[K][N] % 1000000000)
