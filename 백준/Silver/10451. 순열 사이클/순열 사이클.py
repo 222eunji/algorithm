f0923d14fe0567e5d45f6f161098f8c30b9dcd4c
eunji <aggee212@gmail.com>
2026-03-21 09:19:28 +0900

    [Silver III] Title: 순열 사이클, Time: 140 ms, Memory: 111492 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,26 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-def solve():
+def dfs():
   n = int(input())
-  perm = [0] + list(map(int, input().split()))
-
+  perm = [0] + list(map(int,input().split()))
+  cycle_cnt = 0
   visited = [False] * (n+1)
-  cycle_count = 0
-
+  
   for i in range(1, n+1):
-    if not visited[i]:
-      # 새로운 사이클 시작
-      cycle_count += 1
-
+    while not visited[i]:
+      cycle_cnt += 1
       current = i
+
       while not visited[current]:
         visited[current] = True
         current = perm[current]
 
-  print(cycle_count)
+  print(cycle_cnt)
 
 T = int(input())
 for _ in range(T):
-  solve()
-  
+  dfs()
