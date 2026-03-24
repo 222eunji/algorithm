4bdecc30dd63d0cd56d66bf504af18f22a9c816e
eunji <aggee212@gmail.com>
2026-03-24 22:57:21 +0900

    [Gold IV] Title: 이분 그래프, Time: 720 ms, Memory: 185476 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,43 +1,42 @@
-from collections import deque
-import sys;
+import sys
 input = sys.stdin.readline
 
-
-def bfs(start):
-    q = deque([start])
-    color[start] = 1
-
-    while q:
-        node = q.popleft()
-
-        for next_node in graph[node]:
-            if color[next_node] == 0:
-                color[next_node] = -color[node]
-                q.append(next_node)
-
-            elif color[next_node] == color[node]:
-                return False
-
-    return True
-
-
 T = int(input())
-for tc in range(1, T+1):
+
+def bfs(node):
+    global flag
+    q = [node]
+    mark[node] = 1
+    while q:
+        # num이 1이라면 나머지는 -1, 반대라면 1
+        v = q.pop(0)
+        for n in graph[v]:
+            if not mark[n]:
+                mark[n] = -mark[v]
+                q.append(n)
+            else:
+                if mark[n] == mark[v]:
+                    flag = True
+                    return
+
+for _ in range(T):
     V, E = map(int, input().split())
     graph = [[] for _ in range(V+1)]
+    mark = [0] * (V+1)
+    flag = False
+
+    # 그래프 값 입력
     for _ in range(E):
         s, e = map(int, input().split())
         graph[s].append(e)
         graph[e].append(s)
-    color = [0] * (V+1)
-
-
-
-    for idx in range(1, V+1):
-        if color[idx] == 0:
-            if not bfs(idx):
-                print('NO')
+    
+    # 이분 그래프 탐색하기
+    for node in range(1, V+1):
+        if not mark[node]:
+            bfs(node)
+            if flag:
+                print("NO")
                 break
-    else:
-        print('YES')
-
+    if not flag:
+        print("YES")
