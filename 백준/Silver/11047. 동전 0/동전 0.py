b37d15aa34de07eaeb40a3ae31a361f770ca2265
eunji <aggee212@gmail.com>
2026-04-05 15:13:01 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,14 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-
-n, goal = map(int,input().split())
-coin_type = list(map(int, [input() for _ in range(n)]))
-
+N, K = map(int, input().split())
+coins = list(int(input()) for _ in range(N))
 cnt = 0
-for i in range(n-1, -1, -1):
-  if goal >= coin_type[i]:
-    cnt += goal // coin_type[i]
-    goal %= coin_type[i]
+
+for i in range(len(coins)-1, -1, -1):
+    if K >= coins[i]:
+        cnt += K // coins[i]
+        K = K % coins[i]
+        if K == 0:
+            break
 
 print(cnt)
